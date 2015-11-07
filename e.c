adcd7804f7d5957f9fe74c1e255f1bef928a31f1
Julio Moya <julio.moyadom@gmail.com>
2015-11-06 22:53:09 -0600

    Version 0.3

--- e.c
+++ e.c
@@ -10,7 +10,8 @@ void imprime(int *co, int tam);
 int main (int argc, char **argv)
 {
 	int nproc, pid;	
-	int *a, *b, *c, *re, *co,*to, N, i, total;
+	int *a, *b, *c, *re, *co,*to;
+	int x, y, N, i, j, total, tmp, op_terminadas = 0;
 
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &nproc);
@@ -41,7 +42,7 @@ int main (int argc, char **argv)
 				llenamatriz(b, 1, N);
 				llenamatriz(c, 0, N);
 
-				for(i=0; i<nproc-1; i++)
+				/*for(i=0; i<nproc-1; i++)
 				{				
 					MPI_Send(&a[0], 1, renglon, i+1, 1, MPI_COMM_WORLD);
 				}
@@ -54,34 +55,54 @@ int main (int argc, char **argv)
 				for(i=0; i<nproc-1; i++)
 				{				
 					MPI_Recv(&c[i], 1, MPI_INT, i+1, 1, MPI_COMM_WORLD, &status);
-				}			
+				}*/	
+				while(op_terminadas < nproc - 1) {
+					MPI_Recv(&tmp, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG,
+					 MPI_COMM_WORLD, &status);
+					if(status.MPI_TAG == 0) // Tag 0 enviar renglon
+					{
+						int index = N * (tmp * (nproc-1) + status.MPI_SOURCE - 1);
+						MPI_Send(&a[index], 1, renglon, status.MPI_SOURCE, status.MPI_SOURCE, MPI_COMM_WORLD);
+					} 
+					else if(status.MPI_TAG == 1) // Tag 1 enviar columna
+					{
+						MPI_Send(&b[tmp], 1, columna, status.MPI_SOURCE, status.MPI_SOURCE, MPI_COMM_WORLD);
+					}
+					else if(status.MPI_TAG == 2)// Tag 2 es que termino
+					{
+						op_terminadas++;
+					}
+					else // Cualquier otra tag indica la posicion del resultado
+					{
+						x = status.MPI_TAG % 10;
+						y = N * ((status.MPI_TAG / 10 - 1) * (nproc-1) + status.MPI_SOURCE - 1);
+						c[x+y] = tmp;
+					}
+				}
+				imprime(c, N);
 			}
 			else
 			{
 				re=(int *)malloc(sizeof(int) * N);
 				co=(int *)malloc(sizeof(int) * N);
 				to=(int *)malloc(sizeof(int) * N);
-		
-				MPI_Recv(re, 1, renglon, 0, 1, MPI_COMM_WORLD, &status);
-				MPI_Recv(co, 1, renglon, 0, 1, MPI_COMM_WORLD, &status);
 
-				printf("\n");
-				
-				for(i=0; i<N; i++)
-				{				
-					printf("[%02d] ", re[i]);
+				for(i=0; i<N / (nproc-1); i++)
+				{
+					MPI_Send(&i, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);			
+					MPI_Recv(re, 1, renglon, 0, pid, MPI_COMM_WORLD, &status);
+					
+					for(j=0; j<N; j++)
+					{
+						MPI_Send(&j, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
+						MPI_Recv(co, 1, renglon, 0, pid, MPI_COMM_WORLD, &status);
+						
+						total = multiplica(re, co, N);
+						MPI_Send(&total, 1, MPI_INT, 0, (i+1)*10+j, MPI_COMM_WORLD);
+					}
 				}
-
-				printf("\n");
 				
-				for(i=0; i<N; i++)
-				{				
-					printf("[%02d] ", co[i]);
-				}
-
-				total=multiplica(re, co, N);
-				printf("\n\n%d\n\n",total);
-				MPI_Send(&total, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
+				MPI_Send(&total, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
 
 			}			
 		}
