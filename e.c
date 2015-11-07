f78cb18f296760773d16759a2b616295aefc6eb9
Julio Moya <julio.moyadom@gmail.com>
2015-11-06 20:40:36 -0600

    Version 0.2

--- e.c
+++ e.c
@@ -19,68 +19,64 @@ int main (int argc, char **argv)
 	MPI_Status status;
 	MPI_Datatype renglon;
 	MPI_Datatype columna;
-
-		
 	
-	if(argc >1)
+	if(argc > 1)
 	{
 		N = atoi(argv[1]);
 
-		MPI_Type_vector(1,N,N,MPI_INT,&renglon);
-         	MPI_Type_commit (&renglon);
-          	MPI_Type_vector(N,1,N,MPI_INT,&columna);
-          	MPI_Type_commit (&columna);
+		MPI_Type_vector(1, N, N, MPI_INT, &renglon);
+		MPI_Type_commit (&renglon);
+		MPI_Type_vector(N, 1, N, MPI_INT, &columna);
+		MPI_Type_commit (&columna);
 
-		if(N%(nproc-1)==0)
-		{
-			if(pid==0)
+		if(N % (nproc-1) == 0)
+		{ 
+			if(pid == 0)
 			{
-				a=(int *)malloc(sizeof(int)*N*N);
-				b=(int *)malloc(sizeof(int)*N*N);
-				c=(int *)malloc(sizeof(int)*N*N);
+				a = (int *)malloc(sizeof(int) * N * N);
+				b = (int *)malloc(sizeof(int) * N * N);
+				c = (int *)malloc(sizeof(int) * N * N);
 		
 				llenamatriz(a, 1, N);
 				llenamatriz(b, 1, N);
 				llenamatriz(c, 0, N);
 
-				for(i=0; i<nproc-1;i++)
+				for(i=0; i<nproc-1; i++)
 				{				
 					MPI_Send(&a[0], 1, renglon, i+1, 1, MPI_COMM_WORLD);
 				}
 
-				for(i=0; i<nproc-1;i++)
+				for(i=0; i<nproc-1; i++)
 				{				
 					MPI_Send(&b[i], 1, columna, i+1, 1, MPI_COMM_WORLD);
 				}
 					
-				for(i=0; i<nproc-1;i++)
+				for(i=0; i<nproc-1; i++)
 				{				
 					MPI_Recv(&c[i], 1, MPI_INT, i+1, 1, MPI_COMM_WORLD, &status);
-				}
-
-				imprime(a, N);
-				imprime(b, N);
-				imprime(c, N);					
+				}			
 			}
 			else
 			{
-				re=(int *)malloc(sizeof(int)*N);
-				co=(int *)malloc(sizeof(int)*N*N);
-				to=(int *)malloc(sizeof(int)*N);
+				re=(int *)malloc(sizeof(int) * N);
+				co=(int *)malloc(sizeof(int) * N);
+				to=(int *)malloc(sizeof(int) * N);
 		
 				MPI_Recv(re, 1, renglon, 0, 1, MPI_COMM_WORLD, &status);
 				MPI_Recv(co, 1, renglon, 0, 1, MPI_COMM_WORLD, &status);
 
 				printf("\n");
-				for(i=0; i<N;i++)
+				
+				for(i=0; i<N; i++)
 				{				
-					printf("[%02d] ",re[i]);
+					printf("[%02d] ", re[i]);
 				}
 
 				printf("\n");
-				for(i=0; i<N;i++)
+				
+				for(i=0; i<N; i++)
 				{				
-					printf("[%02d] ",co[i]);
+					printf("[%02d] ", co[i]);
 				}
 
 				total=multiplica(re, co, N);
@@ -91,7 +87,7 @@ int main (int argc, char **argv)
 		}
 		else
 		{	
-			if(pid==0)
+			if(pid == 0)
 			{
 				printf("\n\nEl numero de elementos no es divisble entre los procesos\n\n");
 			}
@@ -102,7 +98,7 @@ int main (int argc, char **argv)
 	}
 	else
 	{
-		if(pid==0)
+		if(pid == 0)
 		{
 			printf("\n\nFaltan argumentos para el proyecto\n\n");
 		}
@@ -118,15 +114,19 @@ void llenamatriz(int *v, int va, int tam)
 	int i;
 	for (i=0; i<tam*tam; i++)
 	{
-		if(va==1)
+		if(va == 1)
 		{
-			v[i]=rand()%10;
+			v[i] = (rand() % 10) + 1;
 		}
 		else
 		{
-			v[i]=0;
+			v[i] = 0;
 		}  
 	}
+	
+	if(va != 0)
+		imprime(v, tam);
+	
 	return;
 }
 
@@ -134,9 +134,9 @@ int multiplica(int *re, int *co, int tam)
 {
 	int res=0, i;
 
-	for(i=0; i<tam;i++)
+	for(i=0; i<tam; i++)
 	{
-		res+=(re[i]*co[i]);
+		res += re[i] * co[i];
 	}
 
 	return res;   
@@ -150,11 +150,12 @@ void imprime(int *co, int tam)
 	for (i=0, j=0; i<tam*tam; i++, j++)
 	{
 		printf("[%02d] ", co[i]);
-		if(j==(tam-1))
+		if(j == tam - 1)
 		{
 			printf("\n");
-			j=-1;
+			j = -1;
 		}	
 	}
+
 	return;
 }
